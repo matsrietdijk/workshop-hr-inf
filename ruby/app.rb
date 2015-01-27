0010d59b9889543e4257b80d728ae19631fba7db
Mats Rietdijk <matsrietdijk@gmail.com>
2015-01-27 23:41:13 +0100

    Adds sqlite to Ruby

--- ruby/app.rb
+++ ruby/app.rb
@@ -1,11 +1,28 @@
 require "rubygems"
 require "bundler/setup"
 
-require "sinatra"
+require "sinatra/base"
+require "sinatra/activerecord"
 
-get "/" do
+class Author < ActiveRecord::Base
+  validates_presence_of :name
+end
+
+class Article < ActiveRecord::Base
+  validates_presence_of :title, :content
+end
+
+class App < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+
+  set :database, { adapter: "sqlite3", database: ":memory:" }
+
+  get "/" do
     # the variable "title" containing the text "Hello World!"
     @title = "Hello World!"
     # render the template file "views/index.erb"
     erb :index
+  end
+
+  run!
 end
