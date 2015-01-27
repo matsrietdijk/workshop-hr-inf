a374d4c54b481a1e46892b650b41320f356ae62a
Mats Rietdijk <matsrietdijk@gmail.com>
2015-01-27 22:18:12 +0100

    Adds sqlite db connection to Haskell

--- haskell/app.hs
+++ haskell/app.hs
@@ -2,11 +2,30 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 import Yesod
 import Data.Text
+import Database.Persist
+import Database.Persist.Sqlite
 
 data App = App
+           { connPool :: PersistConfigPool SqliteConf
+           , persistConfig :: SqliteConf
+           }
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Author
+    name Text
+
+Article
+    title Text
+    content Textarea
+    authorId AuthorId
+|]
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
@@ -14,14 +33,24 @@ mkYesod "App" [parseRoutes|
 
 instance Yesod App
 
+instance YesodPersist App where
+    type YesodPersistBackend App = SqlBackend
+    runDB = defaultRunDB persistConfig connPool
+
 getHomeR :: Handler Html
 getHomeR = do
     -- render the template file "views/index.hamlet"
     defaultLayout $(whamletFile "views/index.hamlet")
   where
     -- the function "title" returning the text "Hello World!"
-    title :: Text
-    title = "Hello World!"
+    title = "Hello World!" :: Text
 
 main :: IO ()
-main = warp 3000 App
+main = do
+    pool <- createPoolConfig sqlConf
+    runPool sqlConf (runMigration migrateAll) pool
+    warp 3000 App
+              { connPool = pool
+              , persistConfig = sqlConf
+              }
+  where sqlConf = SqliteConf "workshop.sqlite3" 1
